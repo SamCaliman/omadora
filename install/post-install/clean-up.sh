49dd72d9ca895fe23dbab3721744170c66dbb236
SamCal <samuelcalimangoncalvez@gmail.com>
2026-03-13 15:05:09 -0300

    fix: remove line that deletes NetworkManager configuration files

--- install/post-install/clean-up.sh
+++ install/post-install/clean-up.sh
@@ -6,5 +6,3 @@ sudo dnf group remove -y "${groups[@]}"
 mapfile -t packages < <(grep -v '^#' "$OMADORA_INSTALL/omadora-removed.packages" | grep -v '^$')
 sudo dnf remove -y "${packages[@]}"
 
-# Remove NetworkManager configuration
-sudo rm -rf /etc/NetworkManager
