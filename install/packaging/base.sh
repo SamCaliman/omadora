e577ed14d2ad96a703bcf951df8b8b233a5c4e46
SamCal <samuelcalimangoncalvez@gmail.com>
2026-03-13 14:10:22 -0300

    feat: remove revomval of networkmanager and added allowerasing to install on base fedora workstation

--- install/packaging/base.sh
+++ install/packaging/base.sh
@@ -4,4 +4,4 @@ sudo dnf group install -y "${groups[@]}"
 
 # Install base packages
 mapfile -t packages < <(grep -v '^#' "$OMADORA_INSTALL/omadora-base.packages" | grep -v '^$')
-sudo dnf install -y "${packages[@]}"
+sudo dnf install -y "${packages[@]}" --allowerasing
